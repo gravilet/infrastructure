501b9a9ee963dea0f5cfc2dc4dea178e358dbcf5
gravilet <gravilet@gmail.com>
2017-03-08 02:05:21 +0300

    coding style

--- test/sort_merge_test.cpp
+++ test/sort_merge_test.cpp
@@ -2,10 +2,9 @@
 #include <sort_merge.h>
 
 TEST(Fake_Check, FirstElementIsOne) {
-    std::vector<int> x;
-    x = {7, 9, 2};
+    std::vector<int> x = {7, 9, 2};
 
     // test 1
-    cormen::merge(x, 1, 1, 1);
+    cormen::merge(&x, 1, 1, 1);
     EXPECT_EQ(1, x[0]);
 }
