30ba52e483131f2ca5d46358c73b0ccc683bb532
gravilet <gravilet@gmail.com>
2017-03-08 20:27:59 +0300

    implement 'sort merge'

--- test/sort_merge_test.cpp
+++ test/sort_merge_test.cpp
@@ -2,9 +2,11 @@
 #include <sort_merge.h>
 
 TEST(Fake_Check, FirstElementIsOne) {
-    std::vector<int> x = {7, 9, 2};
-
     // test 1
-    cormen::merge(&x, 1, 1, 1);
-    EXPECT_EQ(1, x[0]);
+    std::vector<int> x_001 = {7, 9, 2};
+    std::vector<int> x_001_sorted;
+    x_001_sorted = x_001;
+    sort(x_001_sorted.begin(), x_001_sorted.end());
+    cormen::sort_merge(&x_001);
+    EXPECT_EQ(x_001_sorted, x_001);
 }
