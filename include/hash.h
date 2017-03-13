2f6d1fa4db5e130cbc2c3ea938346a1c01ad70a1
gravilet <gravilet@gmail.com>
2017-03-13 20:59:09 +0300

    refactoring

--- include/hash.h
+++ include/hash.h
@@ -7,12 +7,17 @@
 
 namespace gravilet {
 
+
 class BaseHash {
  protected:
-    int64_t collection_size;
-    static int64_t calculate_hash_function(std::string key,
-                                            int64_t collection_size);
-    explicit BaseHash(int64_t size = 100) : collection_size(size) {}
+    size_t static const init_hash_size = 100;
+    // for 'string-key' implementation of hash-function based on division on 26
+    size_t static const hash_function_divisor = 26;
+    size_t collection_size;     // size of hash, not a number of elements
+    static size_t calculate_hash_function(std::string key,
+                                          size_t collection_size);
+    explicit BaseHash(size_t size = init_hash_size) : collection_size(size) {}
+
  public:
     virtual void insert(std::string key, std::string value) = 0;
     virtual void erase(std::string key) = 0;
@@ -21,46 +26,23 @@ class BaseHash {
 
 class OpenAddressingHash : BaseHash {
  private:
-    // bool=true => not empty, not deleted
     std::vector<std::tuple<bool, std::string, std::string>> collection;
-    int64_t step;                           // step of Linear Probing
-    // log26(collection_size) - number of significant letters in key
-    // int64_t hash_support;
-    int64_t current_size;
-    int64_t expand_step = collection_size;  // ha-ha-ha!!!
- public:
-    OpenAddressingHash() : BaseHash(100), collection(100), step(1), current_size(0) {}
-    OpenAddressingHash(int64_t size, int64_t step) : BaseHash(100), collection(100),
-                                                     step(step), current_size(0) {}
-    void expand_collection(int64_t new_size);
-    // int64_t static calculate_hash_function(std::string key);
-    void insert(std::string key, std::string value);
-    void erase(std::string key);
-    std::string find(std::string key) const;
-    int64_t find_hash(std::string key) const;
-    int64_t size();
+    size_t step;                           // step of Linear Probing
+    size_t current_size;
+    size_t expand_step = collection_size;
+    void expand_collection(size_t new_size);
+    size_t find_hash(std::string key) const;
+
+public:
+    OpenAddressingHash(size_t size = init_hash_size, size_t step = 1) : BaseHash(size),
+                                                               collection(size),
+                                                               step(step),
+                                                               current_size(0) {}
+    void insert(std::string key, std::string value) override;
+    void erase(std::string key) override;
+    std::string find(std::string key) const override;
+    size_t size();
 };
-//
-//    struct ListNode {
-//        std::string key;
-//        std::string value;
-//        ListNode *next;
-//        ListNode(std::string k, std::string v) : key(k), value(v), next(NULL) {}
-//    };
-//
-//    class ChainingHash : BaseHash {
-//     private:
-//        std::vector<int64_t, ListNode*> collection;
-//     public:
-//        ChainingHash() : collection_size(100) {}
-//        void expand_collection(int64_t new_size);
-//
-//        // int64_t calculate_hash_function(std::string key);
-//        void insert(std::string key, std::string value);
-//        void erase(std::string key);
-//        std::string find(std::string key) const;
-//        int64_t size();
-//    };
 
 }  // namespace gravilet
 
--- test/hash_test.cpp
+++ test/hash_test.cpp
@@ -3,9 +3,25 @@
 
 TEST(OpenAddressingHash, CheckDuplicate) {
     gravilet::OpenAddressingHash a;
-    a.insert("20", "aaa");
-    a.insert("20", "bbb");
+    a.insert("aaa", "Alice");
+    a.insert("aaa", "Berta");
 
-    EXPECT_EQ("bbb", a.find("20"));
+    // check correct overwriting the value
+    EXPECT_EQ("Berta", a.find("aaa"));
+    // check that number of elements dont changet after overwriting
     EXPECT_EQ(1, a.size());
+}
+
+TEST(OpenAddressingHash, CheckOverrateSizeOfCollection) {   // if still 'hash_function_divisor = 26'
+    gravilet::OpenAddressingHash a(10); // 10 < hash_function_divisor
+    a.insert("aaa", "Alice1");
+    a.insert("bbb", "Berta");
+    a.insert("kkk", "Alice2");  // 'a' + 10 == 'k'
+    a.insert("kkk", "Alice3");
+    a.insert("aaa", "Alice4");
+
+    EXPECT_EQ("Alice4", a.find("aaa"));
+    EXPECT_EQ("Berta", a.find("bbb"));
+    EXPECT_EQ("Alice3", a.find("kkk"));
+    EXPECT_EQ(3, a.size());
 }
